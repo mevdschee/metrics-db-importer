c56dd1a11b66736e968674d20d0612913bcdf79c
Maurits van der Schee <maurits@vdschee.nl>
2024-09-29 12:22:54 +0200

    update

--- main.go
+++ main.go
@@ -396,9 +396,8 @@ func main() {
 	urlToScrape := flag.String("scrape", "http://localhost:9999/", "single URL to scrape for Gob metrics")
 	scrapeEvery := flag.Duration("every", 1*time.Second, "seconds to wait between scrape requests")
 	retentionInDays := flag.Int("retention", 30, "retention in days")
-	//driverName := flag.String("db", "postgres", "")
-	//dataSourceName := flag.String("dsn", "dbname=metrics sslmode=disable user=metrics password=metrics search_path=public", "")
 	driverName := flag.String("db", "mysql", "drivername, either 'postgres' or 'mysql'")
+	//dataSourceName := flag.String("dsn", "dbname=metrics sslmode=disable user=metrics password=metrics search_path=public", "dsn for the driver, see go sql documentation")
 	dataSourceName := flag.String("dsn", "metrics:metrics@unix(/var/run/mysqld/mysqld.sock)/metrics", "dsn for the driver, see go sql documentation")
 	flag.Parse()
 
